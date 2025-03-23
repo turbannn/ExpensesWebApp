e89b354a5aba63678c87212de91375600f5ed381
SyrаxX- -xx <gera8337@gmail.com>
2025-03-23 20:35:28 +0100

    Logging refactored

--- ExpenseWebAppDAL/Data/WebAppContext.cs
+++ ExpenseWebAppDAL/Data/WebAppContext.cs
@@ -2,12 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using ExpenseWebAppDAL.Configuration;
 using ExpenseWebAppDAL.Interfaces;
-using ExpenseWebAppDAL.Interceptors;
+using Microsoft.Extensions.Logging;
 
 namespace ExpenseWebAppDAL.Data
 {
     public class WebAppContext : DbContext
     {
+        private static readonly string LogFilePath = "efcore_sql_logs.txt";
         public DbSet<Expense> Expenses { get; set; } = null!;
         public DbSet<Category> Categories { get; set; } = null!;
 
@@ -31,18 +32,31 @@ namespace ExpenseWebAppDAL.Data
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.AddInterceptors(new SqlLoggerInterceptor());
+            optionsBuilder.LogTo(AppendLogToFile, LogLevel.Information);
 
             base.OnConfiguring(optionsBuilder);
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             modelBuilder.ApplyConfiguration(new ExpenseConfiguration());
 
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
 
             base.OnModelCreating(modelBuilder);
+        }
+        private static void AppendLogToFile(string logMessage)
+        {
+            try
+            {
+                using (var writer = new StreamWriter(LogFilePath, true))
+                {
+                    writer.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {logMessage}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка записи в лог: {ex.Message}");
+            }
         }
     }
 }
